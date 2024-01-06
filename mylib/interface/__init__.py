bb6414ba72e30373e223739606d99d840c17a6b7
Matheus <matheushnt06@gmail.com>
2024-01-06 10:44:02 -0300

    feat: definindo as funções para o sistema

---
+++ mylib/interface/__init__.py
@@ -0,0 +1,29 @@
+def ler_inteiro():
+    try:
+        val = int(input('Sua opção: '))
+    except (ValueError, TypeError):
+        print('Digite uma opção válida.')
+    except KeyboardInterrupt:
+        print('o usuário preferiu não informar opções.')
+    else:
+        return val
+
+
+def linha(tam=45):
+    print('='*tam)
+
+
+def cabecalho(ttl='título indefinido'):
+    linha()
+    print(ttl.upper().center(45))
+    linha()
+
+
+def menu(lst):
+    cont = 1
+    for item in lst:
+        print(f'{cont} - {item}')
+        cont += 1
+    linha()
+    opc = ler_inteiro()
+    return opc
