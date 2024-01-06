1718b12f657474eb4cbd0bdf38b4d2bfbb3fce45
Matheus <matheushnt06@gmail.com>
2024-01-06 12:12:43 -0300

    FIM: Mini Sistema de Cadastro de Pessoas criado de forma simples

--- main.py
+++ main.py
@@ -1,15 +1,20 @@
 from mylib.interface import *
+from mylib.files import *
+
+file = 'cadastro_de_pessoas.txt'
+if not arquivo_existe(file):
+    criar_arquivo(file)
 
 while True:
     cabecalho('sistema de cadastro')
     resp = menu(['Cadastrar Nova Pessoa', 'Listar Pessoas', 'Sair do Sistema'])
     if resp == 1:
         cabecalho('novo cadastro')
-        nome = str(input('Nome: '))
+        nome = str(input('Nome: ')).strip().capitalize()
         idade = int(input('Idade: '))
+        cadastrar(file, nome, idade)
     elif resp == 2:
-        # ler_arquivo(file)
-        pass
+        ler_arquivo(file)
     elif resp == 3:
         cabecalho('saindo do sistema. até logo 😉')
         break
